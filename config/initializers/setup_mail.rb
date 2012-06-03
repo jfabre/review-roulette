dc8c9d4ae75df913beb798b2efe0965830af8d89
Jeremy Fabre <jeremy.fabre@hotmail.com>
2012-06-03 01:54:21 -0400

    fixed mail and kept interceptor for prod tests. Fixed layouts.

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,8 +1,9 @@
-ActionMailer::Base.smtp_settings = {
-  :address => "relais.videotron.ca",
-  :port => 25,
-  :openssl_verify_mode => 'none'
-}
-
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) if Rails.env.development?
+if Rails.env.development?
+  ActionMailer::Base.smtp_settings = {
+    :address => "relais.videotron.ca",
+    :port => 25,
+    :openssl_verify_mode => 'none'
+  }
+  ActionMailer::Base.default_url_options[:host] = "localhost:3000"  
+end
+ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) 
