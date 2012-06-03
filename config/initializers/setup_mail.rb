381cab6a1e6e26caa351807710c67f55de60c426
Jeremy Fabre <jeremy.fabre@hotmail.com>
2012-06-03 02:17:02 -0400

    fix of setup_mail initializer for migrations

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,9 +1,11 @@
-if Rails.env.development?
-  ActionMailer::Base.smtp_settings = {
-    :address => "relais.videotron.ca",
-    :port => 25,
-    :openssl_verify_mode => 'none'
-  }
-  ActionMailer::Base.default_url_options[:host] = "localhost:3000"  
+if defined?(DevelopmentMailInterceptor)
+  if Rails.env.development?
+    ActionMailer::Base.smtp_settings = {
+      :address => "relais.videotron.ca",
+      :port => 25,
+      :openssl_verify_mode => 'none'
+    }
+    ActionMailer::Base.default_url_options[:host] = "localhost:3000"  
+  end
+  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) 
 end
-ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor) 
